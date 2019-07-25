c87caadf7e376b881844d460fc3b77b3d270c0be
Matheus Solla Recher <matheus.solla.recher@gmail.com>
2019-07-25 15:01:48 -0300

    Add files via upload

---
+++ basico1.py
@@ -0,0 +1,23 @@
+#Para declarar em python não precisa ter ";" no fim.
+
+x = 3
+y = 4
+
+str1="Hakuna matata"
+str2="É lindo dizer"
+
+print (str1 + " " + str2)
+print (y+x)
+# A função print consegue somar numeros e colocar lado a lado strings.
+# Mas se somar uma string com um numero um erro vai aparecer portanto "print (y+str1)" não vai funcionar.
+
+
+print ("Lorem ipsun")
+# É possível printar uma frase direto da função, sem precisar declarar nenhuma variável.
+
+
+#Exemplo de soma de strings
+nome1="Clebin Das Lei "
+
+print ("Parabéns, "+ nome1 + "você ganhou um tekpix 2007" )
+
